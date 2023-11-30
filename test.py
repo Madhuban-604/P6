6ea8328c7875bb8e7d061edd9a3512d9ded2430b
Madhuban44 <86342081+Madhuban44@users.noreply.github.com>
2023-11-30 10:57:25 +0530

    Added

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from main import to_upper
 
 class MyTestCase(unittest.TestCase):
     def test(self):
-        nam='Madhuban'
+        name='Madhuban'
         upper_name=to_upper(name)
         self.assertEqual(upper_name,'MADHUBAN')
 
